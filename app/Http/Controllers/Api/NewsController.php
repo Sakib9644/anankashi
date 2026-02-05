c0ac9d78d1b4e1477ab5b4521a7f1b81ab2dea0c
Sakib9644 <17203075@iubat.edu>
2026-02-05 15:30:05 +0600

    popurl_news_upted

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -329,7 +329,6 @@ class NewsController extends Controller
                 })->values(),
             ];
         });
-        // Replace collection with transformed data
 
 
         return response()->json([
